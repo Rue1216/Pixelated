a4106dc22dcb640236f386ced42f8a7868362e7a
Rue <ltsuyun1999@gmail.com>
2023-09-28 13:50:54 +0800

    adding new objectives

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,15 @@
   <ul>
     <h3>這次學會了...</h3>
     <li>使用 JSON Placeholder 模擬 api 資料串接</li>
-    <li>Svelte 的動態路由寫法</li>
+    <li>Svelte 的動態路由和 redirect 寫法</li>
+  </ul>
+  <br />
+  <ul>
+    <h3>之後預計要調整的</h3>
+    <li>SEO 設定</li>
+    <li>想嘗試使用 GTM 加入 GA4 追蹤的設定</li>
+    <li>想引進 TailwindCSS、將目前的樣式修改為 RWD</li>
+    <li>嘗試使用 webpack 進行檔案壓縮與編譯</li>
   </ul>
   <br />
   <ul>
